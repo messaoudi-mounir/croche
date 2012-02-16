1e7ba7e922f7a0b15ccda92df27dbd0756f928a1
conoroche <conoroche@gmail.com>
2012-02-16 08:38:53 +0000

    0.2-SNAPSHOT release copy for tag maven-custom-version-plugin-0.2

---
+++ maven-custom-version-plugin/tags/maven-custom-version-plugin-0.2/src/main/java/croche/maven/plugin/version/VersionGeneratorFactory.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2011 Conor Roche
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package croche.maven.plugin.version;
+
+/**
+ * The VersionGeneratorFactory represents a static factory for getting a version generator
+ * @version $Id$
+ * @author conorroche
+ */
+public final class VersionGeneratorFactory {
+
+	/**
+	 * This creates a VersionGeneratorFactory
+	 */
+	private VersionGeneratorFactory() {
+		super();
+	}
+
+	/**
+	 * This creates an instance of a version generator
+	 * @return The version generator
+	 */
+	public static final VersionGenerator createVersionGenerator() {
+		return new VersionGeneratorImpl();
+	}
+
+}
